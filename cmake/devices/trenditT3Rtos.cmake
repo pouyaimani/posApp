9b8bedca596f319e92d49e7efd8df508559d62aa
pouya imani <imanipouyaa@gmail.com>
2025-12-25 18:28:54 +0330

    fix(cmake): fix build commands
    	add lv_env_custom.cmake

--- cmake/devices/trenditT3Rtos.cmake
+++ cmake/devices/trenditT3Rtos.cmake
@@ -4,46 +4,72 @@ if(NOT DEFINED ENV{TRENDIT_SDK_PATH})
     message(FATAL_ERROR "You must set TRENDIT_SDK_PATH before including trendit.cmake")
 endif()
 
-set(TRENDIT_SDK_ROOT ENV{TRENDIT_SDK_PATH})
+set(TRENDIT_SDK_ROOT $ENV{TRENDIT_SDK_PATH})
+set(TRENDIT_APP_ROOT $ENV{TRENDIT_PLATFORM_PATH}/app)
+set(TRENDIT_LIB_SDK_ROOT $ENV{TRENDIT_SDK_LIB_PATH})
 
 #-------------------------------
 # Includes
 #-------------------------------
-set(DEVICE_SDK_INCLUDE_DIRS 
-    ENV{TRENDIT_SDK_LIB_PATH}/inc
-    ENV{TRENDIT_PLATFORM_PATH}/ddi
+set(DEVICE_PLATFORM_INCLUDE_DIRS 
+    $ENV{TRENDIT_PLATFORM_PATH}
+    ${TRENDIT_APP_ROOT}
+    ${TRENDIT_APP_ROOT}/lib
+    ${TRENDIT_APP_ROOT}/lib/dev
+    ${TRENDIT_APP_ROOT}/lib/disp
+    ${TRENDIT_APP_ROOT}/lib/trans
+    ${TRENDIT_APP_ROOT}/lib/transmit
+    ${TRENDIT_APP_ROOT}/lib/utils
+    ${TRENDIT_APP_ROOT}/lv_i18n
+
+    ${TRENDIT_LIB_SDK_ROOT}/inc
+
+    ${TRENDIT_LIB_SDK_ROOT}/libsdkemv/emvlibsrc/dllemv/
+    ${TRENDIT_LIB_SDK_ROOT}/libsdkemv/emvlibsrc/dllemvv20/inc/
+    ${TRENDIT_LIB_SDK_ROOT}/libsdk/libsdkemv/emvlibsrc/dllemvv20/src/
+
+
+    $ENV{TRENDIT_PLATFORM_PATH}/lv_port
+    $ENV{TRENDIT_SDK_LIB_PATH}/inc
+    $ENV{TRENDIT_PLATFORM_PATH}/ddi
 )
 
-#-------------------------------
-# Sources
-#-------------------------------
-# file(GLOB_RECURSE TRENDIT_SOURCES
-#     ${CMAKE_SOURCE_DIR}/src/app/*.c
-#     ${CMAKE_SOURCE_DIR}/src/platform/rtos/*.c
-# )
+set(DEVICE_SDK_SOURCE_DIRS 
+    ${TRENDIT_APP_ROOT}
+    ${TRENDIT_APP_ROOT}/lib/dev
+    ${TRENDIT_APP_ROOT}/lib/disp
+    ${TRENDIT_APP_ROOT}/lib/trans
+    ${TRENDIT_APP_ROOT}/lib/transmit
+    ${TRENDIT_APP_ROOT}/lib/utils
+    ${TRENDIT_APP_ROOT}/lv_i18n
+    $ENV{TRENDIT_PLATFORM_PATH}/lv_port
+    $ENV{TRENDIT_PLATFORM_PATH}
+)
 
-# target_sources(${PROJECT_NAME} PRIVATE ${TRENDIT_SOURCES})
+set(DEVICE_SDK_SOURCES)
+
+foreach(dir ${DEVICE_SDK_SOURCE_DIRS})
+    file(GLOB_RECURSE SDK_SRC CONFIGURE_DEPENDS
+        ${dir}/*.c
+        ${dir}/*.S
+    )
+    list(APPEND DEVICE_SDK_SOURCES ${SDK_SRC})
+endforeach()
 
 #-------------------------------
 # Compile definitions
 #-------------------------------
 set(DEVICE_COMPILE_DEFINITIONS 
-    __MICROLIB
-    POS_PLATFORM_RTOS
+    D__MICROLIB
+    LV_CONF_INCLUDE_SIMPLE
+    LV_LVGL_H_INCLUDE_SIMPLE
 )
 
-#-------------------------------
-# Linker flags
-#-------------------------------
-set(DEVICE_LINK_FLAGS 
-    --cpu Cortex-M0
-    --strict
-    --scatter ${SCATTER_FILE}
-)
 
 #-------------------------------
 # Libraries
 #-------------------------------
-# target_link_libraries(${PROJECT_NAME} PRIVATE
-#     ${TRENDIT_SDK_PATH}libsdk1.0.18/libtrenditsdk.a
-# )
+set(DEVICE_SDK_LIBRARIES 
+    ${TRENDIT_SDK_ROOT}/libsdk1.0.18/libtrenditsdk.a
+    ${TRENDIT_SDK_ROOT}/EMVL2.lib
+)
--- cmake/toolchains/trendit_armcc.cmake
+++ cmake/toolchains/trendit_armcc.cmake
@@ -8,11 +8,13 @@ set(CMAKE_LINKER       armlink.exe)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 # Toolchain / global compiler flags
-set(CMAKE_C_FLAGS_INIT "--c99 --cpu=Cortex-M0 -Ospace --apcs=interwork --split_sections --no-multibyte-chars --diag_suppress=177 --diag_suppress=1")
+set(CMAKE_C_FLAGS_INIT "--c99 --gnu -c --cpu Cortex-M0 -W -O2 -Ospace --apcs=interwork --split_sections -D__UVISION_VERSION=\"526\" --no-multibyte-chars")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS_INIT}" CACHE STRING "C compiler flags" FORCE)
 
-set(CMAKE_CXX_FLAGS_INIT "--c99 --cpu=Cortex-M0 -Ospace --apcs=interwork --split_sections --no-multibyte-chars --diag_suppress=177 --diag_suppress=1")
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS_INIT}" CACHE STRING "CXX compiler flags" FORCE)
-
-set(CMAKE_ASM_FLAGS_INIT "--cpu=Cortex-M0 --apcs=interwork")
+set(CMAKE_ASM_FLAGS_INIT "--cpu Cortex-M0 --apcs=interwork --pd \"__MICROLIB SETA 1\"")
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS_INIT}" CACHE STRING "ASM compiler flags" FORCE)
+
+unset(CMAKE_EXE_LINKER_FLAGS)
+set(CMAKE_EXE_LINKER_FLAGS "--cpu Cortex-M0 --nodebug --strict --scatter ${SCATTER_FILE} 
+--summary_stderr --info summarysizes --map  --info totals --info veneers 
+-o core.axf")
