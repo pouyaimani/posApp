ab09e8d453230f6e8c5407ec6d27f866239ac8a1
pouya imani <imanipouyaa@gmail.com>
2026-01-21 21:30:20 +0330

    fix(disp): clean up and fix issues
    Signed-off-by: pouya imani <imanipouyaa@gmail.com>

--- src/display/display.c
+++ src/display/display.c
@@ -10,7 +10,8 @@ static Display display;
 static Device *dev;
 // Display buffer
 static uint8_t *buffer;
-#define LV_BUFFER_SIZE  (SCREEN_SIZE / 10) * BYTES_PER_PIXEL
+#define BYTE_PER_PIXEL (LV_COLOR_FORMAT_GET_SIZE(LV_COLOR_FORMAT_RGB565)) /*will be 2 for RGB565 */
+#define LV_BUFFER_SIZE  (DISP_HOR_RES * 60) * BYTE_PER_PIXEL
 
 static volatile bool isFlushEnabled;
 
@@ -43,31 +44,39 @@ static void displayInit() {
     initBuffer();
     // init LVGL
     LOG_TRACE("Initializing LVGL ...");
-    lv_init();
-
 #if LV_USE_LOG
     lv_log_register_print_cb(lvLogCb);
 #endif
+    lv_init();
 
     LOG_TRACE("Creating display ...");
     lv_disp = lv_display_create(DISP_HOR_RES, DISP_VER_RES);
-    lv_display_set_buffers(lv_disp, buffer, NULL, LV_BUFFER_SIZE, LV_DISPLAY_RENDER_MODE_PARTIAL);
-    lv_display_set_flush_cb(lv_disp, disp_flush);
+    lv_display_set_default(lv_disp);
+    lv_display_set_buffers(lv_disp, buffer, NULL, (DISP_HOR_RES * 60) , LV_DISPLAY_RENDER_MODE_PARTIAL);
+    lv_display_set_flush_cb(lv_disp, dispFlush);
 
     LOG_TRACE("Loading main screen ...");
     display.screen = lv_obj_create(NULL);
     lv_screen_load(display.screen);
+    lv_obj_set_size(display.screen, DISP_HOR_RES, DISP_VER_RES);
+    lv_obj_set_style_bg_color(display.screen, lv_color_hex(0xe32400), 0);
     LOG_TRACE("Initializing display finished ...");
 }
 
 static void displayUpdate() {
+    // Previous tick
+    static uint32_t ptick = 0;
+    CALL_ONCE(
+        ptick = GET_TICK();
+    );
     // Current tick
-    uint32_t ctick = OOP_CALL(dev, getTick);
-    lv_tick_inc(ctick - ptick);
+    uint32_t ctick = GET_TICK();
+    uint32_t elapsed = ctick - ptick;
+    // LOG_TRACE("elapsed time = %d", elapsed);
+    lv_tick_inc(elapsed);
     ptick = ctick;
-
     // LVGL timer handler
-    lv_timer_handler();
+    lv_task_handler();
 }
 
 OOP_CTOR(Display) {
@@ -79,7 +88,7 @@ OOP_CTOR(Display) {
 
 Display *getDisplay(void) {
     CALL_ONCE(
-        Display_ctor(&display);
+        OOP_CALL_CTOR(Display, &display);
     );
     return &display;
 }
