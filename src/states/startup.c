1e2fc9a2b94b8b49d3c64565185a403db430a133
pouya imani <imanipouyaa@gmail.com>
2026-01-24 19:19:07 +0330

    fix(startup): clean up startup state
    	use to init diveice and app
    
    Signed-off-by: pouya imani <imanipouyaa@gmail.com>

--- src/states/startup.c
+++ src/states/startup.c
@@ -5,10 +5,9 @@
 #include "display.h"
 #include "keypad/keypad.h"
 #include "magReader/magReader.h"
-#include "wifi/wifi.h"
-#include "statusBar/statusBar.h"
 #include "eventloop.h"
 #include "timer.h"
+#include "wifi/wifi.h"
 
 static lv_obj_t *startUpPage;
 static lv_obj_t *label;
@@ -24,6 +23,7 @@ STATE_DEF_ENTER() {
     core->registerCallback(getEventloop()->runCycle);
     core->registerCallback(getTimerHanlder()->runCycle);
     createStatusBar();
+    SM_GOTO(getState(STATE_ID_IDLE));
 }
 
 STATE_DEF_EXIT() {
@@ -34,18 +34,10 @@ STATE_DEF_HANDLE(TimeOutEvent) {
 
 }
 
-STATE_DEF_HANDLE(KeypadEvent) {
-        LOG_TRACE("event successfully is reached. key = %d", ev->key);
-    if (ev->key == KEY_ESC)
-        OOP_CALL(getDevice(), powerOff);
-}
-
 OOP_CTOR(Startup, State *parent, const char *name) {
     LOG_TRACE("Constructing Startup ...");
     OOP_CALL_CTOR(State, self, parent, name);
     self->base.vtable.enter = STATE_ENTER();
     self->base.vtable.exit = STATE_EXIT();
     self->base.vtable.handleTimeout = STATE_HANDLE(TimeOutEvent);
-    self->base.vtable.handleKeypad = STATE_HANDLE(KeypadEvent);
-    self->base.vtable.handleWifi = STATE_HANDLE(WifiEvent);
 }
