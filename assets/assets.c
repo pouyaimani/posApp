509f01d4b1434dd5c02354996579029927b76360
pouya imani <imanipouyaa@gmail.com>
2026-01-31 22:25:24 +0330

    feat: add assets
    Signed-off-by: pouya imani <imanipouyaa@gmail.com>

---
+++ assets/assets.c
@@ -0,0 +1,33 @@
+#include "assets.h"
+#include <stdlib.h>
+#include "logger.h"
+
+//app address in ram
+#define MCU_BASE_ADDR  (0x1000000)
+//end
+
+#define LVGL_START_ADDR  0x1D8000
+static const char *getImgVer(void)
+{
+    return (const char *)(LVGL_START_ADDR + MCU_BASE_ADDR);
+}
+
+typedef int (*lvgl_func_t)(char *, void **);
+void *getImgAddr(const char *name)
+{
+    int ret = 0;
+    void *p = NULL;
+    lvgl_func_t fun = *((unsigned int *)(LVGL_START_ADDR + 0x200 + MCU_BASE_ADDR));
+
+    ret = fun(name, &p);
+    LOG_TRACE("image name = %s", name);
+    LOG_TRACE("get image ret = %d", ret);
+    if(ret == 0)
+    {
+        return p;
+    }
+    else
+    {
+        return "";
+    }
+}
---
+++ assets/assets.h
@@ -0,0 +1,8 @@
+#ifndef ASSETS_H_
+#define ASSETS_H_
+
+void *getImgAddr(const char *name);
+
+#define ICON_IDLE_MAIN        getImgAddr("img_pNovin")
+
+#endif
