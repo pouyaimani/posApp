5c28b558c8103ce98fccdbc46609ac7f2eacc447
pouya imani <imanipouyaa@gmail.com>
2026-02-11 13:50:35 +0330

    fix(mag): fix bugs
    Signed-off-by: pouya imani <imanipouyaa@gmail.com>

--- src/hal/magReader/t3Rtos/magReader_t3Rtos.c
+++ src/hal/magReader/t3Rtos/magReader_t3Rtos.c
@@ -4,8 +4,11 @@
 #include "posplatform.h"
 #include "sdkMag.h"
 #include "logger.h"
+#include "sdkemvapp.h"
+#include "sdkUtils.h"
 
-static SDK_MAG_CARD_DATA magData;
+static SDK_ICC_PARAM param;
+static SDK_ICC_CARDDATA data;
 
 static MagReaderErr_t translateSdkErr(int err) {
     MagReaderErr_t magErr = MAG_ERR_NO_SWIPE;
@@ -33,27 +36,58 @@ static void init(MagReader* mag) {
     sdkMagClear();
 }
 
-static void read(MagReader* mag) {
-    int magErr = sdkMagRead(&magData);
-    mag->error = translateSdkErr(magErr);
-    if (mag->data.track2.len != 0) {
-        LOG_TRACE("mag track 2 = %s", magData.track2Data);
-        mag->data.track1.len = magData.track1Len;
-        mag->data.track1.data = magData.track1Data;
-        mag->data.track2.len = magData.track2Len;
-        mag->data.track2.data = magData.track2Data;
-        mag->data.track3.len = magData.track3Len;
-        mag->data.track3.data = magData.track3Data;
+bool isLegalTrack2(s8 *track2)
+{
+    u32 track2Len = 0;
+
+    track2Len = track2 == NULL ? 0 : strlen(track2);
+    if ((track2Len > 0 && track2Len <= 12 * 2) || (track2Len > 19 * 2) || (track2Len == 0))
+    {
+        return false;
     }
-    sdkMagClear();
+    return true;
+}
+
+static s32 libTransAdjustTrack(s8 *track1, s8 *track2, s8 *track3)
+{
+    u32 track1Len = 0, track2Len = 0, track3Len = 0;
+
+    track1Len = track1 == NULL ? 0 : strlen(track1);
+    track2Len = track2 == NULL ? 0 : strlen(track2);
+    track3Len = track3 == NULL ? 0 : strlen(track3);
+
+    if (!isLegalTrack2(track2)) {
+        return SDK_MAG_ERR_INPUT;
+    }
+
+    if (track1Len > 76) track1[76] = '\0';
+    if (track2Len > 19 * 2) track2[38] = '\0';
+    if (track3Len > 52 * 2) track3[104] = '\0';
+
+    return SDK_OK;
+}
+
+static void read(MagReader* mag) {
+    int ret = sdkMagReadData(&param, &data);
+    if (ret == SDK_OK) {
+        ret = libTransAdjustTrack((s8 *)data.asTrack1Data, (s8 *)data.asTrack2Data, (s8 *)data.asTrack3Data);
+        if (ret != SDK_OK) {
+            mag->error = MAG_ERR_DATA_ERR;
+            return;
+        }
+    } else {
+        mag->error = MAG_ERR_NO_SWIPE;
+        return;
+    }
+    mag->error = MAG_ERR_SWIPED;
 }
 
 OOP_CTOR(MagReaderT3Rtos) {
     self->base.vtable.init = init;
     self->base.vtable.read = read;
-    self->base.data.track1.data = magData.track1Data;
-    self->base.data.track2.data = magData.track2Data;
-    self->base.data.track3.data = magData.track3Data;
+    self->base.data.track1.data = data.asTrack1Data;
+    self->base.data.track2.data = data.asTrack2Data;
+    self->base.data.track3.data = data.asTrack3Data;
 }
 
 #endif
