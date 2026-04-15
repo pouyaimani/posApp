07a8998003e8ef588390e9d899b1afc59993d806
pouya imani <imanipouyaa@gmail.com>
2026-04-15 19:18:39 +0330

    fix(file): complete t3rtos file hal
    Signed-off-by: pouya imani <imanipouyaa@gmail.com>

--- src/hal/file/t3Rtos/file_t3Rtos.c
+++ src/hal/file/t3Rtos/file_t3Rtos.c
@@ -3,40 +3,66 @@
 #include "file_t3Rtos.h"
 #include "posplatform.h"
 #include <sdkKey.h>
+#include "sdkemvapp.h"
+#include "sdkFile.h"
+
+static FileErr_t translateSskErr(int err) {
+    FileErr_t ret = FILE_ERR_NONE;
+    switch (err) {
+    case SDK_FILE_CRCERR:
+        ret = FILE_ERR_CRCERR;
+        break;
+    case SDK_FILE_ERROR:
+        ret = FILE_ERROR;
+        break;
+    case SDK_FILE_SEEK_ERROR:
+        ret = FILE_SEEK_ERROR;
+        break;
+    case SDK_FILE_EOF:
+        ret = FILE_ERR_EOF;
+        break;
+    case SDK_FILE_OK:
+        ret = FILE_ERR_OK;
+        break;
+    default:
+        break;
+    }
+    return ret;
+}
 
 static bool isExist(File *self, char *path) {
     return sdkFileIsExist(path);
 }
 
 static FileErr_t create(File *self, char *filename, uint32_t len, uint8_t initValue) {
-    sdkFileCreate(filename, len, initValue);
+    return translateSskErr(sdkFileCreate(filename, len, initValue));
 }
 
 static FileErr_t read(File *self, char *path, uint8_t *pheDest,
                 uint32_t siStart, uint32_t *psiDestLen) {
-    return sdkFileRead(path, pheDest, siStart, psiDestLen);
+    return translateSskErr(sdkFileRead(path, pheDest, siStart, psiDestLen));
 }
 
 static FileErr_t write(File *self, char *path, u8 *pheSrc, u32 siSrcLen) {
-    return sdkFileWrite(path, pheSrc, siSrcLen);
+    return translateSskErr(sdkFileWrite(path, pheSrc, siSrcLen));
 }
 
 static FileErr_t append(File *self, char *path, uint8_t *pheSrc,
                 uint32_t siSrcLen) {
-    return sdkFileAppend(path, pheSrc, siSrcLen);
+    return translateSskErr(sdkFileAppend(path, pheSrc, siSrcLen));
 }
 
 static FileErr_t insert(File *self, char *path, uint8_t *pheSrc,
                 uint32_t siStart, uint32_t siSrclen) {
-    return sdkFileInsert(path, pheSrc, siStart, siSrclen);
+    return translateSskErr(sdkFileInsert(path, pheSrc, siStart, siSrclen));
 }
 
 static FileErr_t delete(File *self, char *path) {
-    return sdkFileDel(path);
+    return translateSskErr(sdkFileDel(path));
 }
 
 static uint32_t getSize(File *self, char *path) {
-    sdkFileGetSize(path);
+    return sdkFileGetSize(path);
 }
 
 static uint32_t getFreeSpace(File *self, char *freeSpace) {
@@ -44,6 +70,15 @@ static uint32_t getFreeSpace(File *self, char *freeSpace) {
 }
 
 OOP_CTOR(FileT3Rtos) {
+    self->base.vtable.append = append;
+    self->base.vtable.create = create;
+    self->base.vtable.delete = delete;
+    self->base.vtable.getFreeSpace = getFreeSpace;
+    self->base.vtable.getSize = getSize;
+    self->base.vtable.insert = insert;
+    self->base.vtable.isExist = isExist;
+    self->base.vtable.read = read;
+    self->base.vtable.write = write;
 }
 
 #endif
