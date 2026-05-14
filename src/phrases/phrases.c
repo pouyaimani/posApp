a10bd8a88641e036f7c4556fb2ba937766cce003
pouya imani <imanipouyaa@gmail.com>
2026-05-14 10:38:22 +0330

    fix(phrases): use hardcode phrases
    Signed-off-by: pouya imani <imanipouyaa@gmail.com>

--- src/phrases/phrases.c
+++ src/phrases/phrases.c
@@ -6,55 +6,57 @@
 #include "logger.h"
 #include "common.h"
 
-static Phrases __phrases;
+// static bool findInJson(const char *phrase, char *out, size_t size, Language_t lang) {
+//     char *jsonData = GET_MEM(4096);
+//     FileErr_t ferr = OOP_CALL(file(), read, PHRASES_JSON_ADDR, jsonData, 0, 4096);
+//     if (ferr != FILE_ERR_OK) {
+//         LOG_ERROR("Error in reading file: %s", PHRASES_JSON_ADDR);
+//         return false;
+//     }
 
-static bool findInJson(const char *phrase, char *out, size_t size, Language_t lang) {
-    char *jsonData = GET_MEM(4096);
-    FileErr_t ferr = OOP_CALL(file(), read, PHRASES_JSON_ADDR, jsonData, 0, 4096);
-    if (ferr != FILE_ERR_OK) {
-        LOG_ERROR("Error in reading file: %s", PHRASES_JSON_ADDR);
-        return false;
-    }
+//     cJSON *root = cJSON_Parse(jsonData);
+//     if (!root) {
+//         LOG_ERROR("Json parse error, file: %s", PHRASES_JSON_ADDR);
+//         return false;
+//     }
 
-    cJSON *root = cJSON_Parse(jsonData);
-    if (!root) {
-        LOG_ERROR("Json parse error, file: %s", PHRASES_JSON_ADDR);
-        return false;
-    }
+//     for (int i = 0; i < cJSON_GetArraySize(root); i++) {
+//         cJSON *item = cJSON_GetArrayItem(root, i);
 
-    for (int i = 0; i < cJSON_GetArraySize(root); i++) {
-        cJSON *item = cJSON_GetArrayItem(root, i);
+//         cJSON *phrs = cJSON_GetObjectItem(item, "phrase");
 
-        cJSON *phrs = cJSON_GetObjectItem(item, "phrase");
+//         if (strcmp(phrs->valuestring, phrase) == 0) {
 
-        if (strcmp(phrs->valuestring, phrase) == 0) {
+//             if (lang == EN) {
+//                 cJSON *name = cJSON_GetObjectItem(phrs, "en");
+//                 snprintf(out, size, "s", name->valuestring);
+//             } else if (lang == FA) {
+//                 cJSON *name = cJSON_GetObjectItem(phrs, "fa");
+//                 snprintf(out, size, "s", name->valuestring);
+//             }
+//             break;
+//         }
+//     }
+//     cJSON_Delete(root);
+//     FREE_MEM(jsonData);
+//     return true;
+// }
 
-            if (lang == EN) {
-                cJSON *name = cJSON_GetObjectItem(phrs, "en");
-                snprintf(out, size, "s", name->valuestring);
-            } else if (lang == FA) {
-                cJSON *name = cJSON_GetObjectItem(phrs, "fa");
-                snprintf(out, size, "s", name->valuestring);
-            }
-            break;
-        }
-    }
-    cJSON_Delete(root);
-    FREE_MEM(jsonData);
-    return true;
-}
+// void getPhrase(const char *phrase, int lang, const char *out, size_t size) {
+//     findInJson(phrase, lang, out, size);
+// }
 
-void getPhrase(const char *phrase, int lang, const char *out, size_t size) {
-    findInJson(phrase, lang, out, size);
-}
+static const PhraseEntry gPhrases[] = {
+#define X(id, en, fa) \
+    [id] = { .text = { [LANG_EN] = en, [LANG_FA] = fa } },
 
-OOP_CTOR(Phrases) {
-    self->getPhrase = getPhrase;
-}
+#include "phrases.def"
+#undef X
+};
 
-Phrases *phrases() {
-    CALL_ONCE(
-        OOP_CALL_CTOR(Phrases, &__phrases);
-    );
-    return &__phrases;
+const char *phraseGet(Phrases_t id, Lang_t lang) {
+    if (id >= PHRASE_COUNT || lang >= LANG_COUNT)
+        return "";
+
+    return gPhrases[id].text[lang];
 }
--- src/phrases/phrases.h
+++ src/phrases/phrases.h
@@ -3,155 +3,24 @@
 
 #include "oop.h"
 
-/**********************************************************************
- *
- *                  Macro Definitions Categorized
- * 
- *          This document categorizes the provided macro 
- *          definitions for clarity and easier management.
- * 
- * 
- **********************************************************************/
+typedef enum {
+    LANG_EN,
+    LANG_FA,
+    LANG_COUNT
+} Lang_t;
 
-/**********************************************************************
- *
- *                  General Terms and Phrases
- *          These are common prompts, statuses, and generic
- *          phrases used throughout the interface:
- * 
- **********************************************************************/
+typedef struct {
+    const char *text[LANG_COUNT];
+} PhraseEntry;
 
-#define PHRASE_PLEASE_WAIT                          "pleaseWait"
-#define PHRASE_NO_RESULT_FOUND                      "noResultFound"
-#define PHRASE_EXTRACTING_DATA                      "extractingData"
-#define PHRASE_ENTER_REF_NUM                        "enterRefNum"
-#define PHRASE_ENTER_TRACE                          "enterTrace"
-#define PHRASE_MERCHANT                             "merchant"
-#define PHRASE_SUPERVISOR                           "supervisor"
-#define PHRASE_CUSTOMER                             "customer"
-#define PHRASE_PLEASE                               "please"
-#define PHRASE_ENTER_SERVER                         "enterServer"
-#define PHRASE_ENTER_PORT                           "enterPort"
-#define PHRASE_INVALID_IP                           "invalidIp"
-#define PHRASE_INVALID_PORT                         "invalidPort"
-#define PHRASE_WIFI                                 "wifi"
-#define PHRASE_GPRS                                 "gprs"
-#define PHRASE_SOUND_STTINGS                        "soundSettings"
-#define PHRASE_ENERGY_SETTINGS                      "energySettings"
-#define PHRASE_RECEIPT_SETTINGS                     "receiptSettings"
-#define PHRASE_BRIGHTNESS_SETTINGS                  "brightnessSettings"
-#define PHRASE_TOUCH_STATUS                         "touchStatus"
-#define PHRASE_UPDATE_DATE_TIME                     "updateDateTime"
-#define PHRASE_ENABLE                               "enable"
-#define PHRASE_DISABLE                              "disable"
-#define PHRASE_POWER_OFF                            "powerOff"
-#define PHRASE_MENU                                 "menu"
-#define PHRASE_CANCEL                               "cancel"
-#define PHRASE_CONFIRM                              "confirm"
+typedef enum {
+#define X(id, textFa, textEn) id,
+#include "phrases.def"
+#undef X
 
-/**********************************************************************
- *
- *                  Financial & Transaction-Related
- *          Terms linked to transaction types, reports, 
- *          and financial info:
- * 
- **********************************************************************/
+    PHRASE_COUNT
+} Phrases_t;
 
-#define PHRASE_AMOUNT                               "amount"
-#define PHRASE_AUTHENTICATION_TYPE                  "authenticationType"
-#define PHRASE_SUCCESSFUL_OPERATION                 "successfulOperation"
-#define PHRASE_UNSUCCESSFUL_OPERATION               "unsuccessfulOperation"
-#define PHRASE_INSUFFICIENT_FUND                    "insufficientFund"
-#define PHRASE_INVALID_PIN                          "invalidPin"
-#define PHRASE_INVALID_CARD                         "invalidCard"
-#define PHRASE_INVALID_TRANSACTION                  "invalidTransaction"
-#define PHRASE_PREPAID_PIN                          "prepaidPin"
-#define PHRASE_BILL_ID                              "billId"
-#define PHRASE_PAYMENT_ID                           "paymentId"
-#define PHRASE_DEBIT_CARD                           "debitCard"
-#define PHRASE_CREDIT_CARD                          "creditCard"
-#define PHRASE_PREPAID_CARD                         "prepaidCard"
-#define PHRASE_CUSTOMERS_SECOND_STATEMENT           "customersSecondStatement"
-#define PHRASE_ANY_DEDUCTION_TO_YOUR_ACCOUNT        "anyDeductionToYourAccount"
-#define PHRASE_REFUND_AMOUNT                        "refundAmount"
-#define PHRASE_LOCAL_DATE_REFUND_TRANSACTION        "localDateRefundTransaction"
-#define PHRASE_LOCAL_TIME_REFUND_TRANSACTION        "localTimeRefundTransaction"
-#define PHRASE_MONEY_REFUNDED_WITHIN_WORKDAY        "moneyRefundedWithinWorkday"
-#define PHRASE_MERCHANTS_NAME                       "merchantsName"
-#define PHRASE_PURCHASE                             "purchase"
-#define PHRASE_BILL_PAYMENT                         "billPayment"
-#define PHRASE_PREPAID_RECHARGE                     "prepaidRecharge"
-#define PHRASE_BALANCE_INQUIRY                      "balanceInquiry"
-#define PHRASE_CUSTOMERS_RECEIPT                    "customersReceipt"
-#define PHRASE_MERCHANTS_RECEIPT                    "merchantsReceipt"
-#define PHRASE_TERMINAL_ID                          "terminalId"
-#define PHRASE_TERMINAL_NUMBER                      "terminalNumber"
-#define PHRASE_MERCHANT_ID                          "merchantId"
-#define PHRASE_APPLICATION_NAME                     "applicationName"
-#define PHRASE_DEVICE_TYPE                          "deviceType"
-#define PHRASE_CARD_NUMBER                          "cardNumber"
-#define PHRASE_ISSUING_BANK                         "issuingBank"
-#define PHRASE_ACQUIRING_BANK                       "acquiringBank"
-#define PHRASE_TRACE_NUMBER                         "traceNumber"
-#define PHRASE_LOCAL_DATE_TRANSACTION               "localDateTransaction"
-#define PHRASE_LOCAL_TIME_TRANSACTION               "localTimeTransaction"
-#define PHRASE_RETRIEVAL_REFERENCE_NUMBER           "retrievalReferenceNumber"
-
-/**********************************************************************
- *
- *                  Report and Transaction Types
- *          Specific report or transaction categories:
- * 
- **********************************************************************/
-
-#define PHRASE_DAILY_REPORT                         "dailyReport"
-#define PHRASE_AGGREGATED_REPORT                    "aggregatedReport"
-#define PHRASE_TRANACTION_DETAILS                   "transactionDetails"
-#define PHRASE_TXN_SALE                             "txnSale"
-#define PHRASE_TXN_BALANCE                          "txnBalance"
-#define PHRASE_TXN_BILL_PAYMENT                     "txnBillPayment"
-#define PHRASE_TXN_PAY                              "txnPay"
-#define PHRASE_TXN_VOUCHER                          "txnVoucher"
-#define PHRASE_TXN_TOP_UP                           "txnTopUp"
-#define PHRASE_ALL_TXNS                             "allTxns"
-
-/**********************************************************************
- *
- *                  Operational/Action Buttons & Interfaces
- *                  Common interface actions
- * 
- **********************************************************************/
-
-#define PHRASE_REPRINT                              "reprint"
-#define PHRASE_UPDATE_APP                           "updateApp"
-#define PHRASE_RESET_MERCHAT_PIN                    "resetMerchatPin"
-#define PHRASE_DEFAULT_SETTINGS                     "defaultSettings"
-#define PHRASE_SERVER_SETTINGS                      "serverSettings"
-#define PHRASE_TMS_SETTINGS                         "tmsSettings"
-
-
-/**********************************************************************
- *
- *                   Miscellaneous/Other Settings & Labels
- *              Less common or context-specific phrases:
- * 
- **********************************************************************/
-
-#define PHRASE_SPECIAL_PROJECTS                    "specialProjects"
-#define PHRASE_SSL                                 "ssl"
-#define PHRASE_KEY_INJECTION                       "keyInjection"
-#define PHRASE_CONFIGURATION                       "configuration"
-#define PHRASE_UPDATE_DATE_TIME                    "updateDateTime"
-#define PHRASE_NETWORK_SETTINGS                    "networkSettings"
-#define PHRASE_POWER_OFF                           "powerOff"
-#define PHRASE_MENU                                "menu"
-#define PHRASE_CANCEL                              "cancel"
-#define PHRASE_CONFIRM                             "confirm"
-
-OOP_CLASS(Phrases) {
-    OOP_METHOD(void, getPhrase, const char *phrase, int lang, const char *out, size_t size);
-};
-
-Phrases *phrases();
+const char *phraseGet(Phrases_t id, Lang_t);
 
 #endif
